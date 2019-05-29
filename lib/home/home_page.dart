c2efd3582628fae7e6d8b604314fd32ae35e121d
zhangqin <sdwfqin@gmail.com>
2019-05-29 10:22:03 +0800

    update: 首页item布局

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import 'package:flutter_wanandroid/home/model/banner/HomeBanner.dart';
@@ -77,19 +79,98 @@ class _BodyViewState extends State<BodyView> {
       padding: EdgeInsets.all(10.0),
       margin: EdgeInsets.fromLTRB(10, index == 0 ? 10 : 0, 10, 10),
       decoration: BoxDecoration(
-          color: Color(0xFFFFFFFF),
-          borderRadius: BorderRadius.circular(8.0),
-          border: Border.all(color: Color(0xFFCCCCCC))),
+          color: Colors.white, borderRadius: BorderRadius.circular(8.0)),
       child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Text(data[index].title,
-                overflow: TextOverflow.ellipsis,
-                maxLines: 2,
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 18.0,
-                )),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                // 作者前边的小icon
+                Image(
+                    image: AssetImage("images/me.png"),
+                    width: 12.0,
+                    height: 12.0,
+                    alignment: Alignment.center),
+                // 作者
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    padding: EdgeInsets.fromLTRB(3, 0, 0, 0),
+                    child: Text(data[index].author,
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1,
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 14.0,
+                        )),
+                  ),
+                ),
+                // 分类
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                        data[index].superChapterName +
+                            "/" +
+                            data[index].chapterName,
+                        textAlign: TextAlign.end,
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1,
+                        style: TextStyle(
+                          color: Colors.blue,
+                          fontSize: 14.0,
+                        )),
+                  ),
+                ),
+              ],
+            ),
+            // 标题
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 5, 0, 5),
+              child: Text(data[index].title,
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18.0,
+                  )),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                // 收藏
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    alignment: Alignment.centerLeft,
+                    child: Image(
+                      image: AssetImage("images/collect_no.png"),
+                      width: 20.0,
+                      height: 20.0,
+                    ),
+                  ),
+                ),
+                // 发布时间
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    alignment: Alignment.centerRight,
+                    child: Text(data[index].niceDate,
+                        textAlign: TextAlign.end,
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1,
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 14.0,
+                        )),
+                  ),
+                ),
+              ],
+            ),
           ]),
     );
   }
